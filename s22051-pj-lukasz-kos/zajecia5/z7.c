162cd48bb605ba844154ea05b7c2dc81c7338658
s22051-pj-lukasz-kos <s22051@pjwstk.edu.pl>
2021-06-25 14:30:26 +0200

    Complete lab 5

--- s22051-pj-lukasz-kos/zajecia5/z7.c
+++ s22051-pj-lukasz-kos/zajecia5/z7.c
@@ -1,11 +1,25 @@
+#include <stdio.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
-
-/*
-1:21:00
-*/
-int main(int argc, char *argv[]) {
-
-
+int main(int argc, char* argv[]) {
+    int i;
+    pid_t master_pid;
+    
+    master_pid = getpid();
+    printf("Master PID: %d\n", master_pid);
+    for(i = 0; i < 100; i++) {
+        if(getpid() == master_pid){
+            if(fork() == 0){
+                printf("i=%d\tPID: %d, PPID: %d\n", i, getpid(), getppid());
+                fflush(stdout);
+            }
+        }
+    }
+    if(master_pid == getpid()){
+        waitpid(0,NULL,0);
+        printf("Rodzic zaczekał, koniec\n");
+    }
     return 0;
 }
