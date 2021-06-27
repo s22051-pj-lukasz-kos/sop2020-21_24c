d9bce0e7008931e3e5dfdb7846f9bfa2cea6ef99
s22051-pj-lukasz-kos <s22051@pjwstk.edu.pl>
2021-06-27 11:28:43 +0200

    Fix and add comments to demos for server and client

--- s22051-pj-lukasz-kos/zajecia6/ingalls_demo/client.c
+++ s22051-pj-lukasz-kos/zajecia6/ingalls_demo/client.c
@@ -2,6 +2,7 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+/* dołączony w celu zastosowania struktury hostent */
 #include <netdb.h> 
 
 /* obsługa błędów */
@@ -14,7 +15,17 @@ int main(int argc, char *argv[]) {
     int sockfd, portno, n;
     /* struktura z adresem serwera */
     struct sockaddr_in serv_addr;
-    /*  */ 
+
+    /* struktura do przechowywania adresu hosta
+    struct  hostent {
+        char    *h_name;         oficjalna nazwa hosta 
+        char    **h_aliases;     lista nazw symbolicznych 
+        int     h_addrtype;      typ adresu hosta, AF_INET 
+        int     h_length;        długość adresu w bitach 
+        char    **h_addr_list;}; wskaźnik na listę adresów sieciowych, które kryją się za nazwą hosta 
+        #define h_addr  h_addr_list[0]   pierwszy adres z listy powyżej, nie musisz go przekazywać, 
+                                            ale możesz się do niego odwołać. 
+    */ 
     struct hostent *server;
     char buffer[256];
 
@@ -22,23 +33,56 @@ int main(int argc, char *argv[]) {
        fprintf(stderr,"usage %s hostname port\n", argv[0]);
        exit(0);
     }
+
+    /* nr portu przejęty z listy argumentów podczas wywołania */
     portno = atoi(argv[2]);
+
+    /* tworzy nowe gniazdo po stronie klienta
+        i zwraca deskryptor pliku do gniazda
+        AF_INET - adres format
+        SOCK_STREAM - połączenie podobne do potoku, ale dwukierunkowe
+        trzeci argument to protocol=0. OS sam dobiera domyślne protokoły
+        (TCP - SOCK_STREAM, UDP - SOCK_DGRAM) */
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+    /* jeśli podczas tworzenia deskryptora pliku do gniazda coś się nie uda, zwróci -1 */ 
     if (sockfd < 0) 
         error("ERROR opening socket");
+
+    /* argv[1] zawiera nazwę hosta w Internecie, np. cheerios@cs.rpi.edu. 
+        gethostbyname tworzy wskaźnik do struktury hostent zawierającą taką nazwę symboliczną */
     server = gethostbyname(argv[1]);
+
+    /* obsługa błędu w razie jakby nie udało się stworzyć nazwy hosta */
     if (server == NULL) {
         fprintf(stderr,"ERROR, no such host\n");
         exit(0);
     }
+
+    /* ustawia wszystkie wartości w buforze na zero */
     bzero((char *) &serv_addr, sizeof(serv_addr));
+
+    /* definiowanie formatu adresu serwera i portu
+        sin_family - format adresu, AF_INET */
     serv_addr.sin_family = AF_INET;
-    bcopy((char *)server->h_addr, 
-         (char *)&serv_addr.sin_addr.s_addr,
-         server->h_length);
+
+    /* funkcja do kopiowania łańcucha znaków */
+    bcopy((char *)server->h_addr, /* kopiuje adres hosta... */
+         (char *)&serv_addr.sin_addr.s_addr, /* ...do elementu struktury definiującej adres IPv4 */
+         server->h_length); /* długość adresu w bitach, bcopy tego wymaga */
+    
+    /* sin_port - numer portu, ale musi być przekazany w formacie network byte order.
+        Dlatego używamy funkcji htons */     
     serv_addr.sin_port = htons(portno);
-    if (connect(sockfd,(struct sockaddr *)&serv_addr,sizeof(serv_addr)) < 0) 
+    
+    /* Następnie należy połączyć gniazdo klienta z adresem serwera używając connect(). */
+    if (connect(sockfd, /* gniazdo po stronie klienta */
+        (struct sockaddr *)&serv_addr, /* adres serwera */
+        sizeof(serv_addr)) /* długość adresu serwera */
+        < 0) /* obsługa błędu, gdy nie da się ustanowić połączenia */
         error("ERROR connecting");
+
+    /* Komunikacja z serwerem. Używamy do tego celu read oraz write opartego o gniazdo klienta */
     printf("Please enter the message: ");
     bzero(buffer,256);
     fgets(buffer,255,stdin);
--- s22051-pj-lukasz-kos/zajecia6/ingalls_demo/server.c
+++ s22051-pj-lukasz-kos/zajecia6/ingalls_demo/server.c
@@ -25,22 +25,24 @@ int main(int argc, char *argv[]) {
         fprintf(stderr,"ERROR, no port provided\n");
         exit(1);
     }
-    /* tworzy nowe gniazdo 
+    /* tworzy nowe gniazdo po stronie serwera
+        i zwraca deskryptor pliku do gniazda
         AF_INET - adres format
         SOCK_STREAM - połączenie podobne do potoku, ale dwukierunkowe
         trzeci argument to protocol=0. OS sam dobiera domyślne protokoły
         (TCP - SOCK_STREAM, UDP - SOCK_DGRAM) */
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    /* tworzy deskryptor pliku do gniazda
-        jeśli coś się nie uda, zwróci -1 */ 
+
+    /* jeśli podczas tworzenia deskryptora pliku do gniazda coś się nie uda, zwróci -1 */ 
     if (sockfd < 0) 
         error("ERROR opening socket");
+
     /* ustawia wszystkie wartości w buforze na zero */
     bzero((char *) &serv_addr, sizeof(serv_addr));
     /* nr portu przejęty z listy argumentów podczas wywołania */
     portno = atoi(argv[1]);
 
-    /* definiowanie adresu 
+    /* definiowanie formatu adresu serwera i portu
         sin_family - format adresu, AF_INET */
     serv_addr.sin_family = AF_INET;
     /* sin_addr - struktura in_addr, zawierająca adres IPv4 hosta. 
@@ -60,6 +62,7 @@ int main(int argc, char *argv[]) {
     /* nasłuch na gniazdo. 5 to maksymalna liczba czekających połączeń 
         (działa na większości systemach) */
     listen(sockfd,5);
+
     clilen = sizeof(cli_addr);
     /* akceptuje połączenie z klientem. Zwraca deskryptor pliku do 
         nowego gniazda, które będzie odpowiadać za komunikację z klientem */
